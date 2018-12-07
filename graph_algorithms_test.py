d664c5a3d01b16536e9356455d68267a8603ce23
Ivan <42812006+ivan-015@users.noreply.github.com>
2018-12-06 19:48:31 -0700

    Fixed test case 1

--- graph_algorithms_test.py
+++ graph_algorithms_test.py
@@ -6,15 +6,16 @@
 # Date of last modification: 12/04/2018
 # The purpose of this program is to test the Graph class and
 # the kruskal minimum spanning tree and topological sort algorithms.
-
+from timeit import default_timer as timer
 from Graph import GraphAL
 
 #   ___1__
-#  /       \
-# 0    3----2---4
+#  /   |   \
+# 0----3----2---4
 #
 
 print("TEST 1:")
+start = timer()
 try:
     # Test algorithms with acyclic, undirected graph
     graph_a = GraphAL(5,False)
@@ -22,6 +23,8 @@ try:
     graph_a.add_edge(1,2,5)
     graph_a.add_edge(2,3,1)
     graph_a.add_edge(4,2,3)
+    graph_a.add_edge(1,3,5)
+    graph_a.add_edge(0,3,7)
     print("Kruskal's algorithm MST:")
     edges = graph_a.kruskal_mst()
     for edge in edges:
@@ -29,9 +32,11 @@ try:
     print(graph_a.topological_sort())
 except Exception as ee:
     print(ee)
+print("Runtime:", round((timer()-start)*1000,4), "milliseconds.")
 
 #  0<----3----->1<-----2
 print("\nTEST 2:")
+start = timer()
 try:
     # Test algorithms with acyclic, directed graph
     graph_b = GraphAL(4,True)
@@ -43,12 +48,13 @@ try:
     graph_b.kruskal_mst()
 except Exception as ee:
     print(ee)
-
+print("Runtime:", round((timer()-start)*1000,4), "milliseconds.")
 
 #          _____>2____>
 #         /            \
 #  0---->1<-------------3
 print("\nTEST 3:")
+start = timer()
 try:
     # Test algorithms with cyclic, directed graph
     graph_b = GraphAL(4,True)
@@ -61,3 +67,4 @@ try:
     graph_b.kruskal_mst()
 except Exception as ee:
     print(ee)
+print("Runtime:", round((timer()-start)*1000,4), "milliseconds.")
